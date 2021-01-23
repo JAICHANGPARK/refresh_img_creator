fbd75e5110dee7ed7eb99d91461c1f2a5e4fd0bd
Dreamwalker <aristojeff@gmail.com>
2021-01-23 19:34:09 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:refresh_img_creator/ui/splash_page.dart';
+
+import 'routes.dart';
+import 'ui/app_home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: SafeArea(
-
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Image.asset("assets/heart_bg.png"),
-            Image.asset("assets/heart_nbg.png"),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      title: '시원짤 만들기',
+      darkTheme: ThemeData.dark(),
+      initialRoute: AppRoutes.SPLASH_PAGE,
+      routes: {
+        AppRoutes.SPLASH_PAGE: (context) => SplashPage(),
+        AppRoutes.MAIN_PAGE: (context) => AppHomePage(),
+      },
     );
   }
 }
